79456069e5a6e87d46eb14546de07fea287d5687
Chien-Tai Cheng <htaiwan@yahoo-inc.com>
2018-10-05 14:18:25 +0800

    update maze init project

---
+++ 5. robot_maze/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 14:12:30 2018
+
+@author: htaiwan
+"""
+
+from Maze import Maze
+
+maze = Maze(maze_size=(8,10), trap_number=3)
+print(maze)
